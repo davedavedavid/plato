fe2ed377715ac00c00e28c1771493dc19c14ef74
zhangshunkang <z50021472@china.huawei.com>
2021-11-18 20:04:18 +0800

    replace cache func

--- examples/nnrt/nnrt_datasource_yolo_utils.py
+++ examples/nnrt/nnrt_datasource_yolo_utils.py
@@ -616,30 +616,76 @@ class LoadImagesAndLabels(object):  # for training/testing
                 gb += self.imgs[i].nbytes
                 pbar.desc = 'Caching images (%.1fGB)' % (gb / 1E9)
 
-    def cache_labels(self, path='labels.cache', prefix=''):
+    def cache_labels(self, path=Path('./labels.cache'), prefix=''):
         # Cache dataset labels, check images and read shapes
         x = {}  # dict
-        pbar = tqdm(zip(self.img_files, self.label_files), desc='Scanning images', total=len(self.img_files))
-        for (img, label) in pbar:
+        nm, nf, ne, nc = 0, 0, 0, 0  # number missing, found, empty, duplicate
+        pbar = tqdm(zip(self.img_files, self.label_files),
+                    desc='Scanning images',
+                    total=len(self.img_files))
+        for i, (im_file, lb_file) in enumerate(pbar):
             try:
-                l = []
-                image = Image.open(img)
-                image.verify()  # PIL verify
-                # _ = io.imread(img)  # skimage verify (from skimage import io)
-                shape = exif_size(image)  # image size
-                assert (shape[0] > 9) & (shape[1] > 9), 'image size <10 pixels'
-                if os.path.isfile(label):
-                    with open(label, 'r') as f:
-                        l = np.array([x.split() for x in f.read().splitlines()], dtype=np.float32)  # labels
-                if len(l) == 0:
+                # verify images
+                im = Image.open(im_file)
+                im.verify()  # PIL verify
+                shape = exif_size(im)  # image size
+                segments = []  # instance segments
+                assert (shape[0] > 9) & (shape[1] >
+                                         9), f'image size {shape} <10 pixels'
+                assert im.format.lower(
+                ) in img_formats, f'invalid image format {im.format}'
+
+                # verify labels
+                if os.path.isfile(lb_file):
+                    nf += 1  # label found
+                    with open(lb_file, 'r') as f:
+                        l = [x.split() for x in f.read().strip().splitlines()]
+                        if any([len(x) > 8 for x in l]):  # is segment
+                            classes = np.array([x[0] for x in l],
+                                               dtype=np.float32)
+                            segments = [
+                                np.array(x[1:],
+                                         dtype=np.float32).reshape(-1, 2)
+                                for x in l
+                            ]  # (cls, xy1...)
+                            l = np.concatenate((classes.reshape(
+                                -1, 1), segments2boxes(segments)),
+                                               1)  # (cls, xywh)
+                        l = np.array(l, dtype=np.float32)
+                    if len(l):
+                        assert l.shape[1] == 5, 'labels require 5 columns each'
+                        assert (l >= 0).all(), 'negative labels'
+                        assert (l[:, 1:] <= 1).all(
+                        ), 'non-normalized or out of bounds coordinate labels'
+                        assert np.unique(
+                            l,
+                            axis=0).shape[0] == l.shape[0], 'duplicate labels'
+                    else:
+                        ne += 1  # label empty
+                        l = np.zeros((0, 5), dtype=np.float32)
+                else:
+                    nm += 1  # label missing
                     l = np.zeros((0, 5), dtype=np.float32)
-                x[img] = [l, shape]
+                x[im_file] = [l, shape, segments]
             except Exception as e:
-                x[img] = None
-                print('WARNING: %s: %s' % (img, e))
+                nc += 1
+                print(
+                    f'{prefix}WARNING: Ignoring corrupted image and/or label {im_file}: {e}'
+                )
+
+            pbar.desc = f"{prefix}Scanning '{path.parent / path.stem}' images and labels... " \
+                        f"{nf} found, {nm} missing, {ne} empty, {nc} corrupted"
+        pbar.close()
+
+        if nf == 0:
+            print(
+                f'{prefix}WARNING: No labels found in {path}. See {help_url}')
 
         x['hash'] = get_hash(self.label_files + self.img_files)
+        x['results'] = nf, nm, ne, nc, i + 1
+        x['version'] = 0.1  # cache version
         np.save(path, x)  # save for next time
+        logging.info(f'{prefix}New cache created: {path}')
         return x
 
     def __len__(self):
