6754e283ad18701cf3b192a2ef75cdde7aa3697b
zhangshunkang <z50021472@china.huawei.com>
2021-11-19 14:50:13 +0800

    fix bug

--- plato/trainers/yolo.py
+++ plato/trainers/yolo.py
@@ -195,7 +195,7 @@ class Trainer(basic.Trainer):
                 else:
                     pred = self.model.forward_from(imgs, cut_layer)
                 
-                print("Prediction dim ", pred.shape, flush=True)
+                # print("Prediction dim ", pred.shape, flush=True)
                 loss, loss_items = compute_loss(
                         pred, targets, self.model)  # loss scaled by batch_size
 
