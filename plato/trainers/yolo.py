94063334ebad55d272421937604ce18145699b99
zhangshunkang <z50021472@china.huawei.com>
2021-11-19 15:04:50 +0800

    add hpy

--- plato/trainers/yolo.py
+++ plato/trainers/yolo.py
@@ -81,7 +81,25 @@ class Trainer(basic.Trainer):
                          1)  # accumulate loss before optimizing
         hyp['weight_decay'] *= total_batch_size * accumulate / nbs  # scale weight_decay
 
-
+        hyp.update({'optimizer': 'SGD',  # ['adam', 'SGD', None] if none, default is SGD
+            'lr0': 0.01,  # initial learning rate (SGD=1E-2, Adam=1E-3)
+            'momentum': 0.937,  # SGD momentum/Adam beta1
+            'weight_decay': 5e-4,  # optimizer weight decay
+            'giou': 0.05,  # giou loss gain
+            'cls': 0.5,  # cls loss gain
+            'cls_pw': 1.0,  # cls BCELoss positive_weight
+            'obj': 1.0,  # obj loss gain (*=img_size/320 if img_size != 320)
+            'obj_pw': 1.0,  # obj BCELoss positive_weight
+            'iou_t': 0.20,  # iou training threshold
+            'anchor_t': 4.0,  # anchor-multiple threshold
+            'fl_gamma': 0.0,  # focal loss gamma (efficientDet default is gamma=1.5)
+            'hsv_h': 0.015,  # image HSV-Hue augmentation (fraction)
+            'hsv_s': 0.7,  # image HSV-Saturation augmentation (fraction)
+            'hsv_v': 0.4,  # image HSV-Value augmentation (fraction)
+            'degrees': 0.0,  # image rotation (+/- deg)
+            'translate': 0.0,  # image translation (+/- fraction)
+            'scale': 0.5,  # image scale (+/- gain)
+            'shear': 0.0})  # image shear (+/- deg)
         # Sending the model to the device used for training
         self.model.to(self.device)
 
