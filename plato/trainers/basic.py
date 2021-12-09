b812cc888d9b675296cd157580887f4b43575649
z50021472 <shunkang1997@gmail.com>
2021-12-09 19:35:02 +0800

    Load model

--- plato/trainers/basic.py
+++ plato/trainers/basic.py
@@ -87,16 +87,16 @@ class Trainer(base.Trainer):
 
         # select mapping location for pretrained tensor
         # TODO: Add flag in yaml file to sepcify NPU device
-        # npu = 0
-        # device = torch.device("npu:%d" % npu)
-        # ckpt = torch.load(model_path, map_location='cpu')
-        # exclude_weight_prefix = ['model.24.m.0', 'model.24.m.1', 'model.24.m.2']
-        # list_keys = list(ckpt.keys())
-        # for key in list_keys:
-        #     for prefix in exclude_weight_prefix:
-        #         if key.startswith(prefix):
-        #             ckpt.pop(key)
-        # self.model.load_state_dict(ckpt, strict=False)
+        npu = 0
+        device = torch.device("npu:%d" % npu)
+        ckpt = torch.load(model_path, map_location='cpu')
+        exclude_weight_prefix = ['model.24.m.0', 'model.24.m.1', 'model.24.m.2']
+        list_keys = list(ckpt.keys())
+        for key in list_keys:
+            for prefix in exclude_weight_prefix:
+                if key.startswith(prefix):
+                    ckpt.pop(key)
+        self.model.load_state_dict(ckpt, strict=False)
 
     def train_process(self, config, trainset, sampler, cut_layer=None):
         """The main training loop in a federated learning workload, run in
