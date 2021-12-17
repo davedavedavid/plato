925fb55a2a819409f633496781c9a49824284748
z50021472 <shunkang1997@gmail.com>
2021-12-17 21:17:26 +0800

    Change collate function

--- plato/datasources/yolo.py
+++ plato/datasources/yolo.py
@@ -20,7 +20,7 @@ from plato.datasources import base
 
 def collate_fn(batch):
     """Customized version for the MistNet server when cut_layer is used."""
-    img, label, path, shape = zip(*batch)  # transposed
+    img, label = zip(*batch)  # transposed
     for i, l in enumerate(label):
         l[0, :] = i  # add target image index for build_targets()
     return torch.stack(img, 0), torch.cat(label, 1)
