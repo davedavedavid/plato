dcd30f910b51f71ad7ccfe1c16ced24ecd6e73d9
t00413490 <tantaiyunlong@huawei.com>
2022-01-05 17:09:32 +0800

    增加边侧feature数据量

--- examples/nnrt/nnrt_algorithms/mistnet.py
+++ examples/nnrt/nnrt_algorithms/mistnet.py
@@ -41,46 +41,45 @@ class Algorithm(fedavg.Algorithm):
 
         features_shape = self.features_shape()
 
+        for i in range(5):
+            for inputs, targets, *__ in dataset:
+                # assert inputs.shape[1] == Config().data.input_height and inputs.shape[2] == Config().data.input_width, \
+                #     "The input shape is not consistent with the requirement predefined model."
+                # print("Load image is ", inputs, flush=True)
+                # print("Load targets is ", targets, flush=True)
+                # print("Random numpy number is ", np.random.randn(), flush=True)
+                # print("Random randn number is ", random.random(), flush=True)
+                # inputs = cv2.imread("/home/data/model/image1.jpg")
+                # inputs = np.moveaxis(inputs, -1, 0)
+                # inputs = np.reshape(inputs, (1, -1))
+                inputs = inputs.astype(np.float32)
+                inputs = inputs / 255.0  # normalize image and convert image type at the same time
+                # np.save("/home/data/model/test_image.npy", inputs)
+                logits = self.model.forward(inputs)
+                logits = np.reshape(logits, features_shape)
+                # np.save("/home/data/model/test_feat.npy", logits)
+                targets = np.expand_dims(
+                    targets, axis=0
+                )  # add batch axis to make sure self.train.randomize correct
+                # count += 1
+                # logging.info("[Client #%d] Extracting %d features from %s examples.",
+                #          self.client_id, count, len(dataset))
+                if epsilon is not None:
+                    logging.info("epsilon is %d.",epsilon)
+                    logits = unary_encoding.encode(logits)
+                    if callable(_randomize):
+                        logits = self.trainer.randomize(logits, targets, epsilon)
+                    else:
+                        logits = unary_encoding.randomize(logits, epsilon)
+                    # Pytorch is currently not supported on A500 and we cannot convert
+                    # numpy array to tensor
+                    if self.trainer.device != 'cpu':
+                        logits = logits.astype('float16')
+                    else:
+                        logits = logits.astype('float32')
 
-        for inputs, targets, *__ in dataset:
-            # assert inputs.shape[1] == Config().data.input_height and inputs.shape[2] == Config().data.input_width, \
-            #     "The input shape is not consistent with the requirement predefined model."
-            # print("Load image is ", inputs, flush=True)
-            # print("Load targets is ", targets, flush=True)
-            # print("Random numpy number is ", np.random.randn(), flush=True)
-            # print("Random randn number is ", random.random(), flush=True)
-            # inputs = cv2.imread("/home/data/model/image1.jpg")
-            # inputs = np.moveaxis(inputs, -1, 0)
-            # inputs = np.reshape(inputs, (1, -1))
-            inputs = inputs.astype(np.float32)
-            inputs = inputs / 255.0  # normalize image and convert image type at the same time
-            # np.save("/home/data/model/test_image.npy", inputs)
-            logits = self.model.forward(inputs)
-            logits = np.reshape(logits, features_shape)
-            # np.save("/home/data/model/test_feat.npy", logits)
-            targets = np.expand_dims(
-                targets, axis=0
-            )  # add batch axis to make sure self.train.randomize correct
-            # count += 1
-            # logging.info("[Client #%d] Extracting %d features from %s examples.",
-            #          self.client_id, count, len(dataset))
-            if epsilon is not None:
-                logging.info("epsilon is %d.",epsilon)
-                logits = unary_encoding.encode(logits)
-                if callable(_randomize):
-                    logits = self.trainer.randomize(logits, targets, epsilon)
-                else:
-                    logits = unary_encoding.randomize(logits, epsilon)
-                # Pytorch is currently not supported on A500 and we cannot convert
-                # numpy array to tensor
-                if self.trainer.device != 'cpu':
-                    logits = logits.astype('float16')
-                else:
-                    logits = logits.astype('float32')
-
-            for i in np.arange(logits.shape[0]):  # each sample in the batch
-                feature_dataset.append((logits[i], targets[i]))
-
+                for i in np.arange(logits.shape[0]):  # each sample in the batch
+                    feature_dataset.append((logits[i], targets[i]))
 
         toc = time.perf_counter()
         logging.info("[Client #%d] Features extracted from %s examples.",
