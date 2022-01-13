686f456236cb3f4c557145fb0ccedd8bb839f7ef
XinYao1994 <xyao@cs.hku.hk>
2022-01-13 11:57:02 +0800

    Fixed size calculation.

--- plato/clients/base.py
+++ plato/clients/base.py
@@ -239,8 +239,10 @@ class Client:
                 _data = pickle.dumps(data)
                 await self.send_in_chunks(_data)
                 data_size += sys.getsizeof(_data)
-                original_data_size += sys.getsizeof(data[0])
-                original_data_size += sys.getsizeof(data[1])
+                # original_data_size += sys.getsizeof(data[0])
+                # original_data_size += sys.getsizeof(data[1])
+                original_data_size += data[0].nbytes
+                original_data_size += data[1].nbytes
         else:
             _data = pickle.dumps(payload)
             await self.send_in_chunks(_data)
