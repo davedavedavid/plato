309192c0cec2aec85fb00e70cf999172403bca8a
t50022277 <tangchuanming3@huawei.com>
2022-04-07 17:15:52 +0800

    test

--- plato/trainers/mindspore/ms_yolo.py
+++ plato/trainers/mindspore/ms_yolo.py
@@ -29,11 +29,11 @@ from packages.ms_yolov5.src.logger import get_logger
 from packages.ms_yolov5.src.util import AverageMeter
 from packages.ms_yolov5.src.config import ConfigYOLOV5
 from mindspore.ops import operations as P
-from plato.trainers.mindspore import basic
+# from plato.trainers.mindspore import basic
 
 ms.set_seed(1)
 
-class Trainer(basic.Trainer):
+class Trainer():
     """The YOLOV5 trainer."""
     def __init__(self, model=None):
         super().__init__()
