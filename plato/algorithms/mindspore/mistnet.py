5435802a9430cd3c2171c43173a87412cb3f789f
t50022277 <tangchuanming3@huawei.com>
2022-04-08 11:05:47 +0800

    test

--- plato/algorithms/mindspore/mistnet.py
+++ plato/algorithms/mindspore/mistnet.py
@@ -61,9 +61,9 @@ class Algorithm(fedavg.Algorithm):
     def dataset_generator(trainset):
         """The generator used to produce a suitable Dataset for the MineSpore trainer."""
         print('trainset', len(trainset), flush=True)
-        for logit in trainset:
+        for logit, annotation, batch_y_true_0 in trainset:
         # for logit, annotation, batch_y_true_0, batch_y_true_1, batch_y_true_2, batch_gt_box0, batch_gt_box1, batch_gt_box2, img_hight, img_width, input_shape in trainset:
-            print('batch_gt_box2, img_hight, img_width, input_shape', len(logit), flush=True)
+            print('---------------------------', len(logit), len(annotation), len(batch_y_true_0), flush=True)
             target = [annotation.asnumpy(), batch_y_true_0.asnumpy(), batch_y_true_1.asnumpy(), batch_y_true_2.asnumpy(), batch_gt_box0.asnumpy(), batch_gt_box1.asnumpy(), batch_gt_box2.asnumpy(), img_hight.asnumpy(), img_width.asnumpy(), input_shape.asnumpy()]
             yield logit.asnumpy(), target.asnumpy()
 
