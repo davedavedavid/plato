368dee75ed2457831981fb0479c51f7ed6f25ab7
t50022277 <tangchuanming3@huawei.com>
2022-04-08 11:19:32 +0800

    test

--- plato/algorithms/mindspore/mistnet.py
+++ plato/algorithms/mindspore/mistnet.py
@@ -75,5 +75,5 @@ class Algorithm(fedavg.Algorithm):
         """
         feature_dataset = ds.GeneratorDataset(list(
             Algorithm.dataset_generator(trainset)),
-                                              column_names=["image", "label"])
+                                              column_names=["image", "label","batch_y_true_0", "batch_y_true_1","batch_y_true_2", "batch_gt_box0", "batch_gt_box1", "batch_gt_box2", "img_hight", "img_width", "input_shape")
         self.trainer.train(feature_dataset)
