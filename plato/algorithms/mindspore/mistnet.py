7d085070af24026b463df97caea704a8c1fcfbb2
t50022277 <tangchuanming3@huawei.com>
2022-04-08 15:19:42 +0800

    test

--- plato/algorithms/mindspore/mistnet.py
+++ plato/algorithms/mindspore/mistnet.py
@@ -67,10 +67,10 @@ class Algorithm(fedavg.Algorithm):
         target = trainset[1]
 
         annotation, batch_y_true_0, batch_y_true_1, batch_y_true_2, batch_gt_box0, batch_gt_box1, batch_gt_box2, img_hight, img_width, input_shape = target
-        print('---------------------------', len(logit), len(target), len(annotation), len(batch_y_true_0), flush=True)
+        print('---------------------------', len(logit), len(target), len(annotation), len(batch_y_true_0), len(batch_y_true_1),len(batch_y_true_2),len(batch_gt_box0),len(batch_gt_box1),len(batch_gt_box2),len(img_hight), flush=True)
         target = [annotation.asnumpy(), batch_y_true_0.asnumpy(), batch_y_true_1.asnumpy(), batch_y_true_2.asnumpy(), batch_gt_box0.asnumpy(), batch_gt_box1.asnumpy(), batch_gt_box2.asnumpy(), img_hight.asnumpy(), img_width.asnumpy(), input_shape.asnumpy()]
-        print('target', target, flush=True)
-        yield logit.asnumpy(), target.asnumpy()
+        print('target', len(target), flush=True)
+        yield logit.asnumpy(), target
 
     def train(self, trainset, *args):
         """The main training loop used in the MistNet server.
@@ -80,5 +80,15 @@ class Algorithm(fedavg.Algorithm):
         """
         feature_dataset = ds.GeneratorDataset(list(Algorithm.dataset_generator(trainset)),
                       column_names=["image", "label"])      #  "batch_y_true_0", "batch_y_true_1","batch_y_true_2", "batch_gt_box0", "batch_gt_box1", "batch_gt_box2", "img_hight", "img_width", "input_shape"
-        print('feature_dataset: ', len(feature_dataset), feature_dataset, flush=True)
+        print('----------------------feature_dataset: ', len(feature_dataset), feature_dataset, flush=True)
         self.trainer.train(feature_dataset)
+
+
+
+
+
+
+
+
+
+
