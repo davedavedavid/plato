6225a199abaf768b249a755e270280d15e58963e
c59900052 <cuijinkai@huawei.com>
2022-04-16 11:24:31 +0800

    test

--- plato/algorithms/mindspore/mistnet.py
+++ plato/algorithms/mindspore/mistnet.py
@@ -82,12 +82,12 @@ class Algorithm(fedavg.Algorithm):
         Arguments:
         trainset: The training dataset.
         """
-        dataset = Algorithm.dataset_generator(trainset)
+        #dataset = Algorithm.dataset_generator(trainset)
         # feature_dataset= ds.GeneratorDataset(Algorithm.dataset_generator(trainset),
         #                column_names=["image", "label"])
         #feature_dataset = feature_dataset.batch(batch_size, num_parallel_workers=min(4, num_parallel_workers), drop_remainder=True)
         #print('----------------------feature_dataset: ', feature_dataset, flush=True)
-        self.trainer.train(dataset)
+        self.trainer.train(trainset)
 
 
 
--- plato/trainers/mindspore/ms_yolo.py
+++ plato/trainers/mindspore/ms_yolo.py
@@ -52,7 +52,7 @@ class Trainer():
         #     Trainer.run_sql_statement(
         #         "CREATE TABLE IF NOT EXISTS trainers (run_id int)")
 
-    def train(self, dataset, sampler=None, cut_layer=None, cloud_args=None):
+    def train(self, trainset, sampler=None, cut_layer=None, cloud_args=None):
         def parse_args(cloud_args=None):
             """Parse train arguments."""
             parser = argparse.ArgumentParser('mindspore coco training')
@@ -211,14 +211,32 @@ class Trainer():
         cores = multiprocessing.cpu_count()
         num_parallel_workers = int(cores / device_num)
         #print("trainset: ",trainset[1][0],flush=True)
-        column_out_names = ["image", "annotation", "batch_y_true_0", "batch_y_true_1", "batch_y_true_2", "batch_gt_box0",
+        column_out_names = ["image","annotation", "batch_y_true_0", "batch_y_true_1", "batch_y_true_2", "batch_gt_box0",
                         "batch_gt_box1", "batch_gt_box2", "img_hight", "img_width", "input_shape"]
-        feature_dataset = ds.GeneratorDataset(dataset, column_names = column_out_names)
-        L =len(feature_dataset)
-        feature_dataset = feature_dataset.map(operations=L, input_columns=column_out_names,
-                    output_columns=column_out_names, column_order=column_out_names,
-                    num_parallel_workers=min(4, num_parallel_workers), python_multiprocessing=False)
-        feature_dataset = feature_dataset.batch(args.per_batch_size, num_parallel_workers=min(4, num_parallel_workers), drop_remainder=True)
+        feature_dataset= ds.GeneratorDataset(trainset, column_names=["image", "label"])
+        def dataset_order(feature_dataset):
+            for image, label in feature_dataset:
+                logit = image
+                annotation = label[0]
+                batch_y_true_0 = label[1]
+                batch_y_true_1 = label[2]
+                batch_y_true_2 = label[3]
+                batch_gt_box0 = label[4]
+                batch_gt_box1 = label[5]
+                batch_gt_box2 = label[6]
+                img_hight = label[7]
+                img_width = label[8]
+                input_shape = label[9]
+            return logit, annotation, batch_y_true_0, batch_y_true_1, batch_y_true_2, batch_gt_box0,\
+                   batch_gt_box1, batch_gt_box2, img_hight, img_width, input_shape
+
+        feature_dataset = feature_dataset.map(operations=dataset_order(feature_dataset),
+                                              input_columns=["image", "label"],
+                                              output_columns=column_out_names, column_order=column_out_names,
+                                              num_parallel_workers=min(4, num_parallel_workers),
+                                              python_multiprocessing=False)
+        feature_dataset = feature_dataset.batch(args.per_batch_size, num_parallel_workers=min(4, num_parallel_workers),
+                                                drop_remainder=True)
         data_loader = feature_dataset.create_dict_iterator(output_numpy=True, num_epochs=1)
         #print("feature_dataset: ",feature_dataset,flush=True)
         for i, data in enumerate(data_loader):
