5b813b8a62c2f20653c1540c8454dc9a0f97fd27
c59900052 <cuijinkai@huawei.com>
2022-04-20 11:22:59 +0800

    test

--- plato/trainers/mindspore/ms_yolo.py
+++ plato/trainers/mindspore/ms_yolo.py
@@ -60,7 +60,7 @@ class Trainer():
             parser.add_argument('--device_target', type=str, default='Ascend',
                                 help='device where the code will be implemented.')
             # dataset related
-            parser.add_argument('--per_batch_size', default=1, type=int, help='Batch size for Training. Default: 8')
+            parser.add_argument('--per_batch_size', default=8, type=int, help='Batch size for Training. Default: 8')
             # network related
             parser.add_argument('--resume_yolov5', default='/home/data/pretrained/YoloV5_for_MindSpore_0-300_274800.ckpt', type=str,
                                 help='The ckpt file of YOLOv5, which used to fine tune. Default: ""')
@@ -181,7 +181,7 @@ class Trainer():
         if args.resize_rate:
             config.resize_rate = args.resize_rate
 
-        data_size = 1 #len(trainset[0])
+        data_size = 296 #len(trainset[0])
         args.steps_per_epoch = int(data_size / args.per_batch_size / args.group_size)
 
         if not args.ckpt_interval:
@@ -262,5 +262,3 @@ class Trainer():
 
         args.logger.info('==========end training===============')
 
-    # def load_model(self, filename=None):
-    #     return self.model.load_model_train(args=)
