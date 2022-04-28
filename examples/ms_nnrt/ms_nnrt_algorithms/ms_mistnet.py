5dd34deeb525342fc66c88895a871d11be0fa193
c59900052 <cuijinkai@huawei.com>
2022-04-28 16:58:28 +0800

    test

--- examples/ms_nnrt/ms_nnrt_algorithms/ms_mistnet.py
+++ examples/ms_nnrt/ms_nnrt_algorithms/ms_mistnet.py
@@ -51,7 +51,7 @@ class Algorithm(ms_fedavg.Algorithm):
             np.array(anno)
             img_hight = input_size[0]
             img_wight = input_size[1]
-            #print("img5:", img, img.shape, flush=True)
+            print("img:", img, img.shape, flush=True)
             #input_size = [img_hight, img_wight]
             image, annotation, size = multi_scale_trans(img=img, anno=np.array(anno), input_size=input_size,
                                                         mosaic_flag=mosaic_flag)
--- examples/ms_nnrt/ms_nnrt_datasource_yolo_utils.py
+++ examples/ms_nnrt/ms_nnrt_datasource_yolo_utils.py
@@ -187,7 +187,7 @@ class COCOYoloDataset:
             labels4 = np.concatenate(labels4, 0)
             np.clip(labels4[:, :4], 0, 2 * s, out=labels4[:, :4])  # use with random_perspective
         flag = np.array([1])
-        print('img4: ', img4, img4.shape, flush=True)
+        #print('img4: ', img4, img4.shape, flush=True)
         return img4, labels4, input_size, flag
 
     def __getitem__(self, index):
@@ -239,7 +239,7 @@ class COCOYoloDataset:
             # tmp [x_min y_min x_max y_max, label]
             out_target.append(tmp)
         flag = np.array([0])
-        print('img: ', img, img.shape, flush=True)
+        #print('img: ', img, img.shape, flush=True)
         return img, out_target, input_size, flag
 
     def __len__(self):
