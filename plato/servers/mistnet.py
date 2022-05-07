0a559d1887befaf01f90685daf21c20cc2ae39cb
c59900052 <cuijinkai@huawei.com>
2022-05-07 10:26:37 +0800

    TEST

--- plato/servers/mistnet.py
+++ plato/servers/mistnet.py
@@ -34,7 +34,7 @@ class Server(fedavg.Server):
     async def process_reports(self):
         """Process the features extracted by the client and perform server-side training."""
         features = [features for (__, features) in self.updates]
-        #print("features: ", features, len(features), flush=True)
+        print("features: ", features, len(features), flush=True)
         # Faster way to deep flatten a list of lists compared to list comprehension
 		# convert feature dataset from numpy to torch tensor
         feature_dataset_tensor = []
@@ -44,7 +44,7 @@ class Server(fedavg.Server):
                 feature_dataset_tensor = features[0]
             else:
                 feature_dataset_tensor = features
-            #print("feature_dataset_tensor ", feature_dataset_tensor, len(feature_dataset_tensor), flush=True)
+            print("feature_dataset_tensor ", feature_dataset_tensor, len(feature_dataset_tensor), flush=True)
         else:
             feature_dataset = list(chain.from_iterable(features))
             for feature in feature_dataset:
