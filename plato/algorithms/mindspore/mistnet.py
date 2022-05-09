294a6b57af7e0f76c44bfeac829e2c4060c9313b
c59900052 <cuijinkai@huawei.com>
2022-05-09 16:19:04 +0800

    test

--- plato/algorithms/mindspore/mistnet.py
+++ plato/algorithms/mindspore/mistnet.py
@@ -87,10 +87,10 @@ class Algorithm(fedavg.Algorithm):
         cores = multiprocessing.cpu_count()
         num_parallel_workers = int(cores / device_num)
 
-        dataset = dataset.batch(1, num_parallel_workers=min(4, num_parallel_workers),
+        dataset = dataset.batch(8, num_parallel_workers=min(4, num_parallel_workers),
                                         drop_remainder=True)
 
-        dataset = dataset.repeat(2)
+        dataset = dataset.repeat(200)
         # for image,annotation, batch_y_true_0,batch_y_true_1,batch_y_true_2,batch_gt_box0,\
         #           batch_gt_box1,batch_gt_box2,img_hight,img_width,input_shape in dataset:
         #     #print('----image-----: ',image, image.shape, annotation, annotation.shape, flush=True)
--- plato/trainers/mindspore/ms_yolo.py
+++ plato/trainers/mindspore/ms_yolo.py
@@ -210,7 +210,7 @@ class Trainer():
         data_loader = dataset.create_dict_iterator(output_numpy=True, num_epochs=1)
         #for epoch in range(args.max_epoch):
         for i, data in enumerate(data_loader):
-            print("i: ", i, flush=True)
+            #print("i: ", i, flush=True)
             logits = Tensor(data["image"], ms.float32)
             # annotation = Tensor.from_numpy(data["annotation"], ms.float16)
             batch_y_true_0 = Tensor(data["batch_y_true_0"], ms.float32)
@@ -222,7 +222,7 @@ class Trainer():
             img_hight = int(data["img_hight"][0])
             img_width = int(data["img_width"][0])
             input_shape = Tensor(data["input_shape"][0], ms.float32)
-            print("logits: ", logits, logits.shape, flush=True)
+            #print("logits: ", logits, logits.shape, flush=True)
             #print("batch_y_true_0: ", batch_y_true_0, batch_y_true_1, batch_y_true_2, batch_gt_box0, batch_gt_box1, batch_gt_box2, flush=True)
             #print("---logits----: ", logits, logits.shape, flush=True)
 
