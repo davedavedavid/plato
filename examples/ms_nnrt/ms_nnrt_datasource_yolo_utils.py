3bd3af7d5c8ebdedcc9127c99c7b8d503426cfdd
c59900052 <cuijinkai@huawei.com>
2022-05-11 10:23:48 +0800

    TEST

--- examples/ms_nnrt/ms_nnrt_datasource_yolo_utils.py
+++ examples/ms_nnrt/ms_nnrt_datasource_yolo_utils.py
@@ -586,7 +586,7 @@ def _choose_candidate_by_constraints(max_trial, input_w, input_h, image_w, image
             # box_data should have at least one box
             new_ar = float(input_w) / float(input_h) * _rand(1 - jitter, 1 + jitter) / _rand(1 - jitter, 1 + jitter)
             scale = _rand(0.5, 2)
-            print("scale: ", scale, flush=True)
+            #print("scale: ", scale, flush=True)
             if new_ar < 1:
                 nh = int(scale * input_h)
                 nw = int(nh * new_ar)
@@ -690,17 +690,18 @@ def _data_aug(image, box, jitter, hue, sat, val, image_input_size, max_boxes,
                                                       box_data=box_data,
                                                       allow_outside_center=True,
                                                       max_boxes=max_boxes)
+    print("candidate:", candidate, flush=True)
     dx, dy, nw, nh = candidate
-    interp = get_interp_method(interp=10)
-    print("image0:", image, interp, flush=True)
+    interp = 0 #get_interp_method(interp=10)
+    print("image0:", np.asarray(image), interp, flush=True)
     image = image.resize((nw, nh), pil_image_reshape(interp))
-    print("image1:", image, flush=True)
+    print("image1:", nw, nh, np.asarray(image), flush=True)
     # place image, gray color as back graoud
     new_image = Image.new('RGB', (input_w, input_h), (128, 128, 128))
     new_image.paste(image, (dx, dy))
     image = new_image
     #image2 = np.asarray(image)
-    print("image2:", image, flush=True)
+    print("image2:", np.asarray(image), flush=True)
     if flip:
        image = filp_pil_image(image)
     image = np.array(image)
