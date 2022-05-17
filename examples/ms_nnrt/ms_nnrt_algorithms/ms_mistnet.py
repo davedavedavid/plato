3e9756620ea63af36153fd6837ec903dabd1d656
c59900052 <cuijinkai@huawei.com>
2022-05-17 19:10:42 +0800

    test

--- examples/ms_nnrt/ms_nnrt_algorithms/ms_mistnet.py
+++ examples/ms_nnrt/ms_nnrt_algorithms/ms_mistnet.py
@@ -113,11 +113,13 @@ class Algorithm(ms_fedavg.Algorithm):
             #inputs = inputs.astype(np.float32)
             #inputs = np.load("/home/data/home/huawei/tt/data/1/COCO/coco128/img.npy", allow_pickle=True)
             #print("inputs: ", inputs, inputs.shape, flush=True)
-            inputs = dd[index][1]
-            np.save("/home/data/home/huawei/tt/data/1/COCO/coco128/before_forward.npy", inputs)
+            inputs = dd[index][0]
+            if index==1:
+                np.save("/home/data/home/huawei/tt/data/1/COCO/coco128/before_forward.npy", inputs)
             logits = self.model.forward(inputs)
             logits = np.reshape(logits, features_shape)
-            np.save("/home/data/home/huawei/tt/data/1/COCO/coco128/after_forward.npy", logits)
+            if index == 1:
+                np.save("/home/data/home/huawei/tt/data/1/COCO/coco128/after_forward.npy", logits)
             #print("logits: ", logits, logits.shape, flush=True)
             #np.save("/home/data/home/huawei/tt/data/1/COCO/coco128/test_logits_2.npy", logits)
             #print("input_data: ", bbox1, bbox2, bbox3, gt_box1, gt_box2, gt_box3, flush=True)
