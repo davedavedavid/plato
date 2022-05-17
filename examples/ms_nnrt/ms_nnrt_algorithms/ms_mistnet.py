463c7320bf6cb360eac7521df4907ef0d699d7ce
c59900052 <cuijinkai@huawei.com>
2022-05-17 16:38:12 +0800

    test

--- examples/ms_nnrt/ms_nnrt_algorithms/ms_mistnet.py
+++ examples/ms_nnrt/ms_nnrt_algorithms/ms_mistnet.py
@@ -51,7 +51,7 @@ class Algorithm(ms_fedavg.Algorithm):
         # for i in range(5):[[][]]
         #inp = np.load("/home/data/home/huawei/tt/data/1/COCO/coco128/image4.npy", allow_pickle=True)
         #edge_data = []
-        dd = np.load("/home/data/home/huawei/tt/data/1/COCO/coco128/new_data.npy", allow_pickle=True)
+        dd = np.load("/home/data/home/huawei/tt/data/1/COCO/coco128/normalize_data.npy", allow_pickle=True)
         for index, (img, anno, input_size, mosaic_flag) in enumerate(dataset):
             np.array(anno)
             img_hight = input_size[0]
@@ -84,8 +84,6 @@ class Algorithm(ms_fedavg.Algorithm):
             image = np.array(image, dtype='float32')
             #image = inp[index]
             #print("img3:", image, image.shape, flush=True)
-            image = dd[index][0]
-            annotation_x = dd[index][1:]
             mean = [m * 255 for m in [0.485, 0.456, 0.406]]
             std = [s * 255 for s in [0.229, 0.224, 0.225]]
             mean = np.array(mean, dtype=image.dtype)
@@ -101,6 +99,8 @@ class Algorithm(ms_fedavg.Algorithm):
             # img.save("/home/data/home/huawei/tt/data/1/COCO/coco128/annotations/test2.jpg")
             image = image.swapaxes(1, 2).swapaxes(0, 1)  # HWC->HCW->CHW    CV.HWC2CHW  or images.transpose((2,0,1))
             #print("img4", image, image.shape, flush=True)
+            image = dd[index][0]
+            annotation_x = dd[index][1:]
             ds = concatenate(image)
             inputs = ds.astype(np.float32)
             inputs = np.expand_dims(inputs, axis=0)
