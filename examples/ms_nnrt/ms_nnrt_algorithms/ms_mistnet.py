25f4ad1c048381d5ffc1abccf5dd4926e4ad0b35
c59900052 <cuijinkai@huawei.com>
2022-06-01 17:30:39 +0800

    test

--- examples/ms_nnrt/ms_nnrt_algorithms/ms_mistnet.py
+++ examples/ms_nnrt/ms_nnrt_algorithms/ms_mistnet.py
@@ -72,12 +72,12 @@ class Algorithm(ms_fedavg.Algorithm):
 
             if epsilon is not None:
                 logging.info("epsilon is %d.", epsilon)
-                #logits = unary_encoding_1b.encode(logits)
+                logits = unary_encoding_1b.encode(logits)
                 # if callable(_randomize):
                 #     logits = self.trainer.randomize(logits, annotation_x[0], epsilon)
                 #     #print('----logits2----', logits, flush=True)
                 # else:
-                #logits = unary_encoding_1b.randomize(logits, epsilon)
+                logits = unary_encoding_1b.randomize(logits, epsilon)
                     # Pytorch is currently not supported on A500 and we cannot convert
                     # numpy array to tensor
                 # if self.trainer.device != 'cpu':
